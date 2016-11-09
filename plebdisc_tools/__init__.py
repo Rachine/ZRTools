acb3b751342f010e9afe1d88e8cd673325a4d1dc
Juan Benjumea <jubenjum@gmai.com>
2016-11-09 17:13:27 +0100

    install binary files on plebdisc_tools

--- plebdisc_tools/__init__.py
+++ plebdisc_tools/__init__.py
@@ -1,1 +1,4 @@
 """Wrapper for bash commands and a few tools for plebdisc"""
+
+from .plebdisc import compute_percentile_param, fdict, launch_lsh, launch_plebdisc
+
--- setup.py
+++ setup.py
@@ -1,18 +1,49 @@
-# from distutils.core import setup
+"""Setup script for the ZRTools package"""
+
+import os
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 from distutils.extension import Extension
+from subprocess import call
+
 from Cython.Build import cythonize
 import numpy as np
 
+VERSION = '0.1'
 
-setup(
-    name = 'ZRTools',
-    ext_modules = cythonize([
-        Extension("plebdisc_interface.plebdisc_wrapper",
-                  ["plebdisc_interface/plebdisc_wrapper.pyx", "ZRTools/plebdisc/util.c", "ZRTools/plebdisc/second_pass.c", "ZRTools/plebdisc/score_matches.c", "ZRTools/plebdisc/feat.c", "ZRTools/plebdisc/dot.c", "ZRTools/plebdisc/signature.c"],
-                  include_dirs=[".", "plebdisc_interface", "ZRTools/plebdisc/", np.get_include()], extra_compile_args=["-std=c99"]),
-        Extension("plebdisc_interface.py_secondpass",
-                  ["plebdisc_interface/py_secondpass.pyx"],
-                  include_dirs=[np.get_include()])]),
-    packages = find_packages()
+# build ZRTools binary files
+HERE = os.path.dirname(os.path.abspath(__file__))
+bin_dir = os.path.join(HERE, 'plebdisc_tools')
+zrtools_dir = os.path.join(HERE, 'ZRTools/plebdisc')
+cmd_ = 'make DESTDIR={} --directory={} install'.format(bin_dir, zrtools_dir)
+call([cmd_], shell=True)
+
+setup(name = 'ZRTools',
+      version=VERSION,
+      description='Wrapper for ZRTools',
+      url='https://github.com/bootphon/ZRTools',
+      author='Bootphon Team',
+      author_email='zerospeech2017@gmail.com',
+      ext_modules = cythonize([
+          Extension("plebdisc_interface.plebdisc_wrapper",
+                    ["plebdisc_interface/plebdisc_wrapper.pyx",
+                        "ZRTools/plebdisc/util.c", 
+                        "ZRTools/plebdisc/second_pass.c", 
+                        "ZRTools/plebdisc/score_matches.c", 
+                        "ZRTools/plebdisc/feat.c", 
+                        "ZRTools/plebdisc/dot.c", 
+                        "ZRTools/plebdisc/signature.c"],
+                    include_dirs=[".", 
+                        "plebdisc_interface", 
+                        "ZRTools/plebdisc/", 
+                        np.get_include()], 
+                    extra_compile_args=["-std=c99"]),
+          Extension("plebdisc_interface.py_secondpass",
+                    ["plebdisc_interface/py_secondpass.pyx"],
+                    include_dirs=[np.get_include()])]),
+      dependency_links=['https://github.com/bootphon/h5features'],
+      include_package_data=True,
+      packages = find_packages()
 )
+
+
