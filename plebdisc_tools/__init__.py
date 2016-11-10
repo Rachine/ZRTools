1b7be2f02616cadd1d4552d3cab5195b41f8c99f
Juan Benjumea <jubenjum@gmai.com>
2016-11-10 14:02:59 +0100

    mods in plebdisc_tools

--- plebdisc_tools/__init__.py
+++ plebdisc_tools/__init__.py
@@ -2,10 +2,6 @@
 
 from .plebdisc import compute_percentile_param, fdict, launch_lsh, launch_plebdisc
 
-from .precision_match import launch_lsh, launch_job, launch_plebdisc, 
-                             merge_results, do_cosine_similarity, 
-                             do_norm_hamming_sim, read_sigs_remove_sils, 
-                             estimate_recall, compute_percentile_param, 
-                             estimate_similarities_distribution
+from .precision_match import launch_lsh, launch_job, launch_plebdisc, merge_results, do_cosine_similarity, do_norm_hamming_sim, read_sigs_remove_sils, estimate_recall, compute_percentile_param, estimate_similarities_distribution
 
 
