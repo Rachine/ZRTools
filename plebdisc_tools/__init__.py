245c142d92c806c8a36f5a27df9ca1a694fe6045
Juan Benjumea <jubenjum@gmai.com>
2016-11-10 14:22:51 +0100

    corrected indentation bug in __init__

--- plebdisc_tools/__init__.py
+++ plebdisc_tools/__init__.py
@@ -1,14 +1,14 @@
 """Wrapper for bash commands and a few tools for plebdisc"""
 
-from .plebdisc import compute_percentile_param, fdict, 
-                      launch_lsh, launch_plebdisc
+from .plebdisc import ( compute_percentile_param, fdict, 
+                        launch_lsh, launch_plebdisc )
 
-from .precision_match import silentremove, load_gold,
-                             load_matches, find_match_triphones,
-                             count_phone_matches, count_matches,
-                             minimal_achievable_ned, min_levenstein_distance,
-                             levenstein_distance, nb_levestein_distance,
-                             nb_min_levestein_distance
+from .precision_match import ( silentremove, load_gold,
+                               load_matches, find_match_triphones,
+                               count_phone_matches, count_matches,
+                               minimal_achievable_ned, min_levenstein_distance,
+                               levenstein_distance, nb_levestein_distance,
+                               nb_min_levestein_distance )
 
 
 
